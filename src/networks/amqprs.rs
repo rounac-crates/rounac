f6ea6a1324864c5e56294d8f4ce9f591a17f50c9
Christopher Skane <sokrbball@gmail.com>
2026-07-14 18:58:22 -0400

    Refactored open args function to return CalErorr for ergonomics.

--- src/networks/amqprs.rs
+++ src/networks/amqprs.rs
@@ -1,31 +1,55 @@
 //! AMQPRS related utilities
 
-use crate::config::{AsbConfig, NetworkKind};
+use crate::{
+	config::{AsbConfig, NetworkKind},
+	error::CalError,
+};
 use amqprs::connection::OpenConnectionArguments;
 use toml::Value;
 
-/// Get the necessary config params to create AMQP connection.
-pub fn open_args_from_config(name: &str, config: AsbConfig) -> Option<OpenConnectionArguments> {
-	let network = config.networks.get(name)?;
+/// Get the necessary config params to create AMQP connection for `net_name`.
+pub fn open_args_for_net(
+	net_name: &str,
+	config: &AsbConfig,
+) -> Result<OpenConnectionArguments, CalError> {
+	let Some(network) = config.networks.get(net_name) else {
+		return Err(CalError::config_err(format!(
+			"No such network \"{net_name}\""
+		)));
+	};
 
 	// Verify this network is the correct type.
 	if network.kind != NetworkKind::Amqp {
-		return None;
+		return Err(CalError::config_err(format!(
+			"Expected network kind \"amqp\" for network \"{net_name}\""
+		)));
 	}
 
 	// Get parameters
-	let Value::String(host) = network.params.get("host")? else {
-		return None;
-	};
-	let Value::Integer(port) = network.params.get("port")? else {
-		return None;
-	};
-	let Value::String(user) = network.params.get("username")? else {
-		return None;
-	};
-	let Value::String(pass) = network.params.get("password")? else {
-		return None;
-	};
+	let host = match network.params.get("host") {
+		Some(Value::String(s)) => Ok(s),
+		_ => Err(CalError::config_err(format!(
+			"Expected string parameter \"host\" for network \"{net_name}\""
+		))),
+	}?;
+	let port = match network.params.get("port") {
+		Some(Value::Integer(i)) => Ok(i),
+		_ => Err(CalError::config_err(format!(
+			"Expected integer parameter \"port\" for network \"{net_name}\""
+		))),
+	}?;
+	let user = match network.params.get("username") {
+		Some(Value::String(s)) => Ok(s),
+		_ => Err(CalError::config_err(format!(
+			"Expected string parameter \"username\" for network \"{net_name}\""
+		))),
+	}?;
+	let pass = match network.params.get("password") {
+		Some(Value::String(s)) => Ok(s),
+		_ => Err(CalError::config_err(format!(
+			"Expected string parameter \"password\" for network \"{net_name}\""
+		))),
+	}?;
 
-	Some(OpenConnectionArguments::new(host, *port as u16, user, pass))
+	Ok(OpenConnectionArguments::new(host, *port as u16, user, pass))
 }
