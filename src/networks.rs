d75529ba9c50de343416a7b8cf31395c96dfb641
Christopher Skane <sokrbball@gmail.com>
2026-07-15 23:06:16 -0400

    Started on logic for reader creation in AsbConnection.

--- src/networks.rs
+++ src/networks.rs
@@ -3,14 +3,20 @@
 pub mod amqp;
 
 use crate::{
+	Topic,
 	config::{AsbConfig, NetworkKind},
 	error::CalError,
 };
 use amqp::open_args_for_net;
-use amqprs::{callbacks::DefaultConnectionCallback, connection::Connection};
+use amqprs::{
+	callbacks::DefaultConnectionCallback,
+	channel::{BasicConsumeArguments, QueueDeclareArguments},
+	connection::Connection,
+};
+use tokio::runtime::{Handle, Runtime};
 
 pub enum AsbConnection {
-	Amqp(amqp::AmqpAsb),
+	Amqp(Handle, amqp::AmqpAsb),
 	Null,
 }
 impl AsbConnection {
@@ -21,24 +27,84 @@ impl AsbConnection {
 			)));
 		};
 
+		// TODO:
 		let rt = tokio::runtime::Builder::new_current_thread()
 			.enable_all()
 			.build()?;
+		let handle = rt.handle().clone();
 
-		match network.kind {
+		let res = match network.kind {
 			NetworkKind::Amqp => {
 				let open_args = open_args_for_net(&network)?;
-				Ok(AsbConnection::Amqp(rt.block_on(async {
+				let a = rt.block_on(async {
 					let conn = Connection::open(&open_args).await?;
 					conn.register_callback(DefaultConnectionCallback).await?;
 					let chan = conn.open_channel(None).await?;
 
+					// TODO: If config has exchange name, create direct exchange.
+
 					let a = amqp::AmqpAsb { conn, chan };
 
 					Ok::<_, amqprs::error::Error>(a)
-				})?))
+				})?;
+
+				AsbConnection::Amqp(handle, a)
 			}
-			NetworkKind::Null => Ok(AsbConnection::Null),
+			NetworkKind::Null => AsbConnection::Null,
+		};
+
+		// Background thread for this connection that drives the tokio runtime.
+		// Spawned after res to ensure connections have succeeded
+		// TODO: Refactor `AsbConnection` to struct so store things like this easier.
+		let joiner = std::thread::spawn(move || rt.block_on(async { loop {} }));
+
+		Ok(res)
+	}
+
+	pub fn create_reader<T>(
+		&self,
+		topic: &Topic<T>,
+		config: &AsbConfig,
+	) -> Result<AsbReader, CalError> {
+		match self {
+			AsbConnection::Amqp(rt, a) => {
+				// Create a queue for this topic
+				// TODO: Check config for topic prefix and adjust `topic_name` accordingly.
+				let topic_name = topic.name.clone();
+
+				// Prepare arguments
+				// TODO: Check config for whether topic should be exclusive
+				let declare_args = QueueDeclareArguments::transient_autodelete(&topic_name);
+
+				// TODO: Create QueueBindArguments if exchange is used. Bind to default exchange is automatic from declaration.
+
+				// Create consumer object for reader with mpsc channel or shared ring buffer.
+				// amqprs::consumer::AsyncConsumer
+				// TODO: Set auto_ack/no_ack depending on QoS (true for best effort, false for reliable).
+				let consume_args = BasicConsumeArguments::new(&topic_name, "");
+
+				rt.block_on(async {
+					// Declare queue
+					a.chan.queue_declare(declare_args).await?;
+
+					// TODO: Bind queue to exchange if necessary
+
+					// Create consumer for topic (subscribe).
+					//a.chan.basic_consume(consumer, consume_args).await?;
+
+					Ok::<_, amqprs::error::Error>(())
+				})?;
+
+				Err(CalError::other_err("Not implemented".to_string()))
+			}
+			AsbConnection::Null => Ok(AsbReader::Null),
 		}
 	}
 }
+
+pub enum AsbReader {
+	Amqp,
+	Null,
+}
+
+pub enum AsbWriter {}
